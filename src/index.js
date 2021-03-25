d2dc5ce3de1e59f83f7dcd26963242ab818d361d
Rajat Sachan <rajatsachan@Rajats-MacBook-Air.local>
2021-03-25 14:53:29 +0530

    added turns for players and win logic

--- src/index.js
+++ src/index.js
@@ -21,8 +21,13 @@ function Square(props) {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if(squares[i] == null){
+          squares[i] = this.state.xIsNext? 'X' : 'O';
+          this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+          });
+        }
     }
 
     renderSquare(i) {
@@ -32,8 +37,20 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+      setTimeout(() => {
   
+      }, 2000);
+      return (
+        <div>
+          <div className="status">{status}</div>
+          </div>
+      );
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       return (
         <div>
           <div className="status">{status}</div>
@@ -54,6 +71,7 @@ function Square(props) {
           </div>
         </div>
       );
+    }
     }
   }
   
@@ -80,3 +98,22 @@ function Square(props) {
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
